4a242bbeaebb9f7f77aa3244fe95f0a22c3e356d
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2021-08-10 07:08:38 +0600

    Bacteria I

---
+++ 2356.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        d = input()
+        s = input()
+        if s in d:
+            print("Resistente")
+        else:
+            print("Nao resistente")
+    except EOFError:
+        break
